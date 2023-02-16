97ac057eed71aa43f3cde6f9a2fe467959f87d2b
Polzocr <micro.cpt.62@outlook.fr>
2023-02-16 17:00:39 +0100

    erreur position body

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
 import './index.css'
 
 export default function Form() {
+
+    const [form, setForm] = useState({
+        userName: '',
+        password: ''
+    })
+
+    function handleSubmit(){
+        console.log("")
+    }
+
+    function handleChange() {
+        console.log("")
+    }
+
     return (
         <section className='sign-in-content'>
             <i className='fa fa-user-circle sign-in-icon'></i>
             <h1>Sign In</h1>
-            <form>
+
+            <form onSubmit={handleSubmit}>
                 <div className='input-wrapper'>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id='username' />
+                    <input type="text" id='username' value={form.userName} onChange={() => handleChange()}/>
                 </div>
                 <div className='input-wrapper'>
                     <label htmlFor="password">Password</label>
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,10 +6,10 @@ import Footer from '../../components/Footer/index'
 
 export default function Layout(){
     return (
-        <body>
+        <>
             <Header />
             <Outlet />
             <Footer />
-        </body>
+        </>
     )
 }
