124c615d459c6f849f4d0eeeb7c13f89f8b2026d
Polzocr <micro.cpt.62@outlook.fr>
2023-02-17 14:47:30 +0100

    v1 edit

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,15 +1,52 @@
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { selectorProfil } from '../../utils/Redux/selectors'
+import { useSelector, useStore } from 'react-redux'
+import { updatingProfilInfos } from '../../features/profil'
+import { selectorLogin, selectorProfil } from '../../utils/Redux/selectors'
 import './index.css'
 
 export default function Welcome(){
     
+    const store = useStore()
     const profil = useSelector(selectorProfil)
+    const login = useSelector(selectorLogin)
+    
     const [editing, setEditing] = useState(false)
+    const [form, setForm] = useState({
+        firstName: '',
+        lastName: ''
+    })
 
-    function editProfil(){
-        console.log('editing')
+    function handleChange(e) {
+        setForm((prevState) => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    function handleSubmit() {
+        checkEmptyValue()
+        editProfil()
+    }
+
+    function checkEmptyValue() {
+        if (form.firstName === '') {
+            form.firstName = profil.data.firstName
+        }
+        if (form.lastName === '') {
+            form.lastName = profil.data.lastName
+        }
+    }
+
+    function editProfil() {
+        updatingProfilInfos(store, login.data.token, form)
+            .then(() => {
+                const status = store.getState().profil.status
+                if (status !== 'rejected') {
+                    setEditing(false)
+                }
+            })
     }
 
     return (
@@ -22,12 +59,12 @@ export default function Welcome(){
             ) : (
                 <>
                     <h1>Welcome back</h1>
-                    <form action="">
-                        <input type="text" placeholder={profil.data.firstName}/>
-                        <input type="text" placeholder={profil.data.lastName}/>
+                    <form className='edit-form'>
+                        <input type="text" placeholder={profil.data.firstName} name="firstName" value={form.firstName} onChange={handleChange}/>
+                        <input type="text" placeholder={profil.data.lastName} name="lastName" value={form.lastName} onChange={handleChange} />
                     </form>
-                    <button className='edit-button' onClick={() => { editProfil }}>Save</button>
-                    <button className='edit-button' onClick={() => { setEditing(false) }}>Cancel</button>
+                        <button className='edit-button save-button' onClick={handleSubmit}>Save</button>
+                    <button className='edit-button save-button' onClick={() => { setEditing(false) }}>Cancel</button>
                 </>
             )}
         </div>
--- src/features/profil.js
+++ src/features/profil.js
@@ -38,6 +38,40 @@ export async function fetchingProfilInfos(store, token) {
     }
 }
 
+export async function updatingProfilInfos(store, token, userData) {
+    const status = selectorProfil(store.getState()).status
+    if (status === 'updating' || status === 'pending') {
+        return
+    }
+    store.dispatch(actions.fetching())
+    try {
+        const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                firstName: userData.firstName,
+                lastName: userData.lastName
+            })
+        })
+        const data = await response.json()
+        console.log('ici', data)
+        if (!data.body) {
+            throw data.message
+        } else {
+            store.dispatch(actions.resolved(data.body))
+            console.log("Profil modifié")
+
+        }
+    } catch (error) {
+        store.dispatch(actions.updateRejected(error))
+        console.log(error)
+    }
+}
+
 const {actions, reducer} = createSlice({
     name:'profil',
     initialState,
@@ -69,6 +103,14 @@ const {actions, reducer} = createSlice({
                 draft.status = 'rejected'
                 draft.error = action.payload
                 draft.data = null
+                return
+            }
+            return
+        },
+        updateRejected: (draft, action) => {
+            if (draft.status === 'pending' || draft.status === 'updating') {
+                draft.status = 'rejected'
+                draft.error = action.payload
                 return
             }
             return
